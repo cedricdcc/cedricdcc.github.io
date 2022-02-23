77b9da94fbc1a3d95864c27652fb77af6e0f3314
cedricd <cedricdecruw@gmail.com>
2022-02-23 17:08:24 +0100

    Update socialbanner.js

--- src/components/socialbanner.js
+++ src/components/socialbanner.js
@@ -1,16 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/socialbanner.css';
 /* import all the react icons required */
-import {FaTwitch, FaYoutube, FaInstagram, FaTwitterSquare} from 'react-icons/fa';
+import {FaTwitch, FaYoutube, FaInstagram, FaTwitterSquare, FaDiscord, FaTiktok} from 'react-icons/fa';
 import { IconContext } from "react-icons";
-
+/* https://www.npmjs.com/package/unofficial-valorant-api */
 function SocialBanner() {
     return (
         <div class ="socialbanner">
-            <FaTwitch class="socialitem"/>
-            <FaYoutube class="socialitem"/>
-            <FaInstagram class="socialitem"/>
-            <FaTwitterSquare class="socialitem"/>
+            <a href="https://www.twitch.tv/impulsiveempathy" target="_blank"><FaTwitch class="socialitem"/></a>
+            <a href="https://www.youtube.com/channel/UCnffZdI8vzFO1gkLg2cpMuA" target="_blank"><FaYoutube class="socialitem"/></a>
+            <a href="https://www.instagram.com/impulsiveempathy/" target="_blank"><FaInstagram class="socialitem"/></a>
+            <a href="https://twitter.com/ImpulsiveEmp" target="_blank"><FaTwitterSquare class="socialitem"/></a>
+            <a href="https://discord.com/invite/n7dXEKPeNs" target="_blank"><FaDiscord class="socialitem"/></a>
+            <a href="https://www.tiktok.com/@impulsiveempathy?lang=sv-SE" target="_blank"><FaTiktok class="socialitem"/></a>
         </div>
     )
 }
